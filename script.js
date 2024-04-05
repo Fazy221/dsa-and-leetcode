3764a4be6d9c32c88a46c03c1ed5ea8a025ca759
Fazy221 <faizanraza2221@gmail.com>
2024-04-06 02:57:51 +0500

    day 3

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 "use strict";
 
-let obj1 = {
-  name: "Faizan",
-};
-let obj2 = obj1;
-obj1 = {
-  name: "Hassan",
-};
-console.log(obj2);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+let myLinkedList = new LinkedList(4);
+console.log(myLinkedList);
