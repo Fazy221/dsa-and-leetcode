94dff65ca506808faea1f1c79d5154bcd33ea136
Faizan Raza <110770826+Fazy221@users.noreply.github.com>
2024-04-16 16:02:47 +0500

    Get Set Insert Remove method.js

--- script.js
+++ script.js
@@ -64,12 +64,67 @@ class LinkedList {
     temp.next = null;
     return temp;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+  set(index, value) {
+    let temp = this.get(index); // Instead of rewriting get method, will just use it there to get value at asked index
+    if (temp) {
+      temp.value = value; // If value is found on given index then temp.value is changed with asked value and return true otherwise false
+      return true;
+    }
+    return false;
+  }
+  // Reason why we do undefined/return list in get method and true/false in set method bcuz in get method,
+  // My Method
+  // insert(index, value) {
+  //   if (index === 0) return this.unshift(value);
+  //   if (index === this.length) return this.push(value);
+  //   if (index < 0 || index > this.length) return false;
+  //   const newNode = new Node(value);
+  //   let temp = this.get(index - 1);
+  //   let exTempNext = temp.next;
+  //   temp.next = newNode;
+  //   newNode.next = exTempNext;
+  //   this.length++;
+  //   return true;
+  // }
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+    const newNode = new Node(value);
+    let temp = this.get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let before = this.get(index-1);
+    let temp = before.next;
+    before.next = temp.next;
+    temp.next = null; 
+    this.length--;
+  }
 }
-
-const myLinkedList = new LinkedList(69);
+let myLinkedList = new LinkedList(53);
+myLinkedList.push(42);
 myLinkedList.push(52);
-myLinkedList.push(32);
+myLinkedList.push(17);
+myLinkedList.push(23);
 myLinkedList.pop();
 myLinkedList.shift();
-myLinkedList.unshift(41);
+myLinkedList.unshift(4);
+// myLinkedList.set(0, 14);
+myLinkedList.insert(1, 92);
 console.log(myLinkedList);
+// console.log(myLinkedList.get(0).value);
